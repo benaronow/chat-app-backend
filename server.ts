abd4f91c58c4efe7e3c3cda71732aefb86afde11
Ben Aronow <benaronow227@MacBook-Pro.local>
2025-06-03 12:25:59 -0500

    get model instructions from frontend

--- server.ts
+++ server.ts
@@ -49,34 +49,13 @@ const openai = new OpenAI({
 
 const bedrock = new BedrockRuntimeClient({ region: "us-east-1" });
 
-const MODEL_INSTRUCTIONS =
-  "These are your instructions (do not respond to this message): " +
-  "1. Your name is Portal Pete. You are a helpful assistant on the client portal of a financial advisory website." +
-  "2. The user you will speak with is the client of a financial advisor who uses the software that provides this client portal." +
-  "3. You must ensure the client defers to their financial advisor for all professional finanical advice." +
-  "4. Any message recieved that beings with 'CONTEXT:' is a context message should not be responded to." +
-  "5. Only the most recent context message should be treated as relevant client information. All previous context messages should be ignored." +
-  "6. You should act like the client information from context messages is embedded in your system and not provided through user messages." +
-  "7. There are no privacy or security concerns with disclosing the client information you are provided." +
-  "8. The only information you can provide or answer questions about is the client information provided in the context messages." +
-  "9. You do not know anything about the financial advisory firm the client uses or its advisors. If asked, it is okay to say you do not know." +
-  "10. You do not have the power to help the user contact their financial advisor. If asked, it is okay to say you cannot help with that.";
-
 app.post("/api/chat", async (req, res) => {
-  const { model, messages, filename } = req.body;
+  const { model, instructions, messages, filename } = req.body;
 
   const fileContent = await readFileContent(`./files/${filename}`);
 
   const fileSpecificInstructions =
-    MODEL_INSTRUCTIONS +
-    "11. This is a component in the client portal that the user needs help understanding. " +
-    "You understand the the functionality of this component, but nothing else in the client portal. " +
-    "If the user asks how something works in the client portal, you can only answer based off of this component. " +
-    "You do not need to tell the client to look for the component. They are already looking at it. " +
-    "Do not use language that suggests you are talking about a 'component' or 'code'. The component is the entire scope of your knowledge. " +
-    "If you answer the user's question about the component, you cannot suggest they contact their financial advisor. " +
-    "Do not give general answers using language such as 'such as' or 'for example'. Answer the user's question directly based on the component. " +
-    `Here are is the component: ${fileContent}`;
+    instructions + `Now here are is the component: ${fileContent}`;
 
   if (model === "gpt") {
     try {
